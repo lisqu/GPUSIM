09802688c22ba23fec1062aee3ace80ec95d04ee
lisqu <quanchen1986@gmail.com>
2015-10-27 18:28:24 -0400

    remove run.sh

--- run-face.sh
+++ run-face.sh
@@ -12,7 +12,7 @@ bg_service2=(pos ner)
 for tg in ${service[@]}; do
     bg=face
     for b in ${n_bg[@]}; do
-        echo "end_to_end" > simulated/sim-${tg}-${n_bg}-${bg}.csv
+        echo "end_to_end" > simulated/${tg}-${n_bg}-${bg}-sim.csv
             for((i=0;i<50;i++))
             do
                 java -jar sim.jar ${tg} face 1 ${b} 200 1000
@@ -23,7 +23,7 @@ done
 for bg in ${bg_service1[@]}; do
     tg=face
     for b in ${n_bg[@]}; do
-        echo "end_to_end" > simulated/sim-${tg}-${n_bg}-${bg}.csv
+        echo "end_to_end" > simulated/${tg}-${n_bg}-${bg}-sim.csv
             for((i=0;i<50;i++))
             do
                 java -jar sim.jar face ${bg} 1 ${b} 200 2000
@@ -34,7 +34,7 @@ done
 for bg in ${bg_service2[@]}; do
     tg=face
     for b in ${n_bg[@]}; do
-        echo "end_to_end" > simulated/sim-${tg}-${n_bg}-${bg}.csv
+        echo "end_to_end" > simulated/${tg}-${n_bg}-${bg}-sim.csv
             for((i=0;i<50;i++))
             do
                 java -jar sim.jar face ${bg} 1 ${b} 200 9999
